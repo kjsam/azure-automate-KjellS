e9855999734eed2dfd6e67a70ac9d8afcda0b7a3
kjsam <github@kjells.net>
2022-02-01 07:15:47 +0100

    Oppgave 3 ferdig

--- src/Oppgave_3.ps1
+++ src/Oppgave_3.ps1
@@ -4,3 +4,9 @@ Clear-Host
 $response = Invoke-WebRequest -Uri $Url
 $cards = $response.Content | ConvertFrom-Json
 
+$kortstokk = @()
+foreach ($card in $cards) {
+    $kortstokk += $card.suit[0] + $card.value
+}
+
+Write-Host "Kortstokk: $kortstokk"
