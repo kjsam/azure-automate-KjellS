2a1fe6c3a2af0c1b44407da908c2330a299dcf01
kjsam <github@kjells.net>
2022-02-01 09:02:05 +0100

    Oppgave 5

--- src/Oppgave_5.ps1
+++ src/Oppgave_5.ps1
@@ -18,10 +18,21 @@ catch {
 
 $cards = $response.Content | ConvertFrom-Json
 
+$sum = 0
+foreach ($card in $cards) {
+    $sum += switch ($card.value){
+        'J' {10}
+        'Q' {10}
+        'K' {10}
+        'A' {11}
+        Default {$card.value}
+    }
+}
+
+# Skriver ut kortstokk
 $kortstokk = @()
 foreach ($card in $cards) {
     $kortstokk += $card.suit[0] + $card.value
 }
-
 Write-Host "Kortstokk: $kortstokk"
-
+Write-Host "Poengsum: $sum"
